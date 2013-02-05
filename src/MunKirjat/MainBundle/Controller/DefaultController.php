ddd3dae1e0583a84137a56eed8d40d153facb3eb
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2013-02-05 23:51:45 +0200

    added modernizr, normalize

--- src/MunKirjat/MainBundle/Controller/DefaultController.php
+++ src/MunKirjat/MainBundle/Controller/DefaultController.php
@@ -5,5 +5,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-
+    public function indexAction()
+    {
+        return $this->render('MunKirjatMainBundle:Default:index.html.twig');
+    }
 }
